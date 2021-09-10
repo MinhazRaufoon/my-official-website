db71ec0b6d867a62314e94153315b9a1e5e9497e
Minhaz Raufoon <minhaz.raufoon@s2019.tu-chemnitz.de>
2021-09-10 22:56:03 +0200

    Remove career interest, education, work history and skill section because they are redundant to my resume

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -4,19 +4,8 @@ import useFetchListFromDB from "../../hooks/useFetchListFromDB"
 import { useContext } from "react"
 import { SettingsContext } from "../../contexts"
 import Loader from "../../components/Loader"
-import {
-  AboutMe,
-  AppSettings,
-  Skill,
-  Education,
-  JobExperience,
-  CareerInterest,
-} from "../../global-types"
+import { AboutMe, AppSettings } from "../../global-types"
 import Introduction from "./Introduction"
-import CareerInterestSection from "./CareerInterest"
-import EducationHistorySection from "./EducationHistory"
-import SkillSection from "./SkillSection"
-import JobExperiencesSection from "./JobExperiences"
 
 export default function Home() {
   const settings: AppSettings = useContext(SettingsContext)
@@ -27,40 +16,13 @@ export default function Home() {
   const { list: photos, isFetching: isPhotosFetching } =
     useFetchListFromDB<string>(`photos`)
 
-  const { list: skills, isFetching: isSkillsFetching } =
-    useFetchListFromDB<Skill>(`skills`)
-
-  const { list: careerInterests, isFetching: isCareerInterestFetching } =
-    useFetchListFromDB<CareerInterest>(`${settings.lang}/careerInterests`)
-
-  const { list: educationHistory, isFetching: isEducationHistoryFetching } =
-    useFetchListFromDB<Education>(`${settings.lang}/educationHistory`)
-
-  const { list: workHistory, isFetching: isWorkHistoryFetching } =
-    useFetchListFromDB<JobExperience>(`${settings.lang}/workHistory`)
-
-  if (
-    isAboutFetching ||
-    isPhotosFetching ||
-    isSkillsFetching ||
-    isCareerInterestFetching ||
-    isEducationHistoryFetching ||
-    isWorkHistoryFetching
-  )
-    return <Loader />
+  if (isAboutFetching || isPhotosFetching) return <Loader />
 
   const { subtitle, summary }: AboutMe = aboutMe
 
   return (
     <main className={styles.Home}>
       <Introduction subtitle={subtitle} photos={photos} summary={summary} />
-
-      <section className={styles.qualificationSection}>
-        <JobExperiencesSection history={workHistory} />
-        <EducationHistorySection history={educationHistory} />
-        <SkillSection skills={skills} />
-        <CareerInterestSection interests={careerInterests} />
-      </section>
     </main>
   )
 }
